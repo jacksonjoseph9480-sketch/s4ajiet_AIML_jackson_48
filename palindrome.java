de8a39440acbf6917e92ee9d2bdc62c9684675ec
Jackson <jacksonjoseph9480@gmail.com>
2026-05-05 13:57:51 +0530

    palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+
+        if (original.equalsIgnoreCase(reversed)) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("Not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
